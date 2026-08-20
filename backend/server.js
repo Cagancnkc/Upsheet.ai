471674050bc70f81f7067d3d2317da13cc2a5c2c
Cagancnkc <cagancnkc@gmail.com>
2026-08-20 07:20:19 +0300

    fix(csp): localizercdn.com backend CSP'ye eklendi, Localizer.init defensive guard eklendi

--- backend/server.js
+++ backend/server.js
@@ -135,7 +135,7 @@ app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: false 
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy',
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.clarity.ms https://scripts.clarity.ms https://www.googletagmanager.com https://www.google-analytics.com; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.clarity.ms https://scripts.clarity.ms https://www.googletagmanager.com https://www.google-analytics.com https://localizercdn.com https://secure.localizer.co; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
     "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net; " +
     "img-src 'self' data: blob: https://cdn.jsdelivr.net https://cdn.simpleicons.org https://svgl.app https://upload.wikimedia.org; " +
